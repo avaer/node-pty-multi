8e475940e3992231b4d0b4f3dcd1100a5d7453f2
Adrian Biedrzycki <a@modules.io>
2017-12-28 05:47:30 -0800

    Add linux prebuilt

--- index.js
+++ index.js
@@ -6,5 +6,18 @@ if (abiVersion !== ABI_VERSION) {
   throw new Error(`Wrong ABI version: got ${abiVersion}, expected ${ABI_VERSION}`);
 }
 
-const pty = process.platform !== 'win32' ? require('node-pty') : require('node-pty-win');
+let pty = null;
+switch (process.platform) {
+  case 'linux': {
+    pty = require('node-pty-linux');
+    break;
+  }
+  case 'win32': {
+    pty = require('node-pty-win');
+    break;
+  }
+}
+if (pty === null) {
+  throw new Error(`Platform not supported: ${process.platform}`);
+}
 module.exports = pty;
