947ebc0b235e1af36c19a391e2a35b781cc07f93
Adrian Biedrzycki <a@modules.io>
2017-12-28 05:39:06 -0800

    Check required abi version

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const ABI_VERSION = '57';
+
+const nodeAbi = require('node-abi');
+const abiVersion = nodeAbi.getAbi();
+if (abiVersion !== ABI_VERSION) {
+  throw new Error(`Wrong ABI version: got ${abiVersion}, expected ${ABI_VERSION}`);
+}
+
 const pty = process.platform !== 'win32' ? require('node-pty') : require('node-pty-win');
 module.exports = pty;
